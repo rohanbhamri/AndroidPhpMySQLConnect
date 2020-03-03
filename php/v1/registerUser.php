ca596a370f2ca96d066259e561c295cc4ccc36b5
Rohan Bhamri <rohan.bhamri03@gmail.com>
2020-03-03 13:29:21 +0530

    register controller

---
+++ php/v1/registerUser.php
@@ -0,0 +1,31 @@
+<?php
+require_once '../includes/DbOperations.php';
+    $response = array();
+
+    if($_SERVER['REQUEST_METHOD'] == 'POST'){
+
+        if(isset($_POST['fullname']) and isset($_POST['email']) and isset($_POST['password'])){
+
+            $db = new DbOperations();
+
+            if($db->createUser($_POST['fullname'],$_POST['email'],$_POST['password'])){
+                $response['error'] = false;
+                $response['message'] = 'User Registered Successfully';
+            }
+            else{
+                $response['error'] = true;
+                $response['message'] = 'User Registeration Failed';
+            }
+
+
+        }
+        else{
+            $response['error'] = true;
+            $response['message'] = 'Required feilds are missing';
+        }
+
+    }
+    else{
+        $response['error'] = true;
+        $response['message'] = "Invalid Request";
+    }
