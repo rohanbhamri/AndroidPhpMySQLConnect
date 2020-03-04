66d396db182076e18a29eac9fc54f35efbffa6a9
Rohan Bhamri <rohan.bhamri03@gmail.com>
2020-03-04 12:15:44 +0530

    android

---
+++ android/app/src/main/java/com/dcubetech/phpmysqlconnect/Constants.java
@@ -0,0 +1,14 @@
+package com.dcubetech.phpmysqlconnect;
+/*
+* Create the class for constants that will be used to send the request.
+*
+* This class is used for getting the pages of your php script.
+* */
+public class Constants {
+
+    //    root url
+    private static final String ROOT_URL = "http://192.168.220.1/AndroidPhpConnect/AndroidPhpMySQLConnect/php/v1/";
+
+    //    php pages.
+    public static final String URL_REGISTER = ROOT_URL+"registerUser.php";
+}
--- php/v1/registerUser.php
+++ php/v1/registerUser.php
@@ -16,16 +16,15 @@ require_once '../includes/DbOperations.php';
                 $response['error'] = true;
                 $response['message'] = 'User Registeration Failed';
             }
-
-
         }
         else{
             $response['error'] = true;
             $response['message'] = 'Required feilds are missing';
         }
-
     }
     else{
         $response['error'] = true;
         $response['message'] = "Invalid Request";
     }
+
+    echo json_encode($response);
